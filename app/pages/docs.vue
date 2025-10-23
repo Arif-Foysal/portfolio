c7e525ca8f3ccce1ca966b3e8215dfcfb9d81216
Arif Foysal <wayne11nayem@gmail.com>
2025-10-23 18:29:15 +0600

    ai chatbox added

--- app/pages/docs.vue
+++ app/pages/docs.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+
+import { HaloSearch } from '~/components/ui/halo-search'
+
+</script>
+
 <template>
-    <!-- <iframe src="https://arif.it.com" class="w-full h-full" frameborder="0"></iframe> -->
+  <div class="w-full flex flex-col items-center justify-center h-96 relative z-0">
+
+    <HaloSearch class=""/>
+
+    <span class="mt-24 text-muted-foreground font-light">
+      Try hovering over component and typing in it.
+    </span>
+  </div>
 </template>
