47fa84b380cbae194955010a0a71c1deee3b64ba
Arif Foysal <wayne11nayem@gmail.com>
2025-10-23 14:36:57 +0600

    css fixes

--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -4,14 +4,12 @@ import { CardSpotlight } from '~/components/ui/card-spotlight'
 import { computed } from "vue";
 import { useColorMode } from "@vueuse/core";
 
-const isDark = computed(() => useColorMode().value == "dark");
 </script>
 
 <template>
   <div class="flex h-[500px] w-full flex-col gap-4 lg:h-[250px] lg:flex-row">
     <CardSpotlight
-      class="cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl"
-      :gradient-color="isDark ? '#363636' : '#C9C9C9'"
+      class="cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl bg-[#C9C9C9] dark:bg-[#363636]"
     >
       Tools that I love work with
     </CardSpotlight>
