229b4615602bf3bba5d62f5b13539eeff281ee99
Arif Foysal <wayne11nayem@gmail.com>
2025-11-01 23:30:43 +0600

    some social links given

--- app/components/SocialIcons.vue
+++ app/components/SocialIcons.vue
@@ -1,17 +1,17 @@
 <template>
     <UFieldGroup size="xl">
-        <UButton icon="mdi:github" size="xl"  color="neutral" variant="outline" square to="https://github.com/yourusername"
+        <UButton icon="mdi:github" size="xl"  color="neutral" variant="outline" square to="https://github.com/arif-foysal"
             target="_blank" class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
         <UButton icon="logos:google-gmail" size="xl" color="neutral" variant="outline" square
-            to="https://twitter.com/yourusername" target="_blank"
+            to="mailto:ariffaysal.nayem@gmail.com" target="_blank"
             class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
         <UButton icon="logos:whatsapp-icon" size="xl" color="neutral" variant="outline" square
-            to="https://linkedin.com/in/yourusername" target="_blank"
+            to="https://wa.me/8801707820797" target="_blank"
             class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
         <UButton icon="logos:linkedin-icon" size="xl" color="neutral" variant="outline" square
-            to="https://linkedin.com/in/yourusername" target="_blank"
+            to="https://www.linkedin.com/in/md-arif-foysal-nayem-206979215/" target="_blank"
             class=" transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
-        <UButton icon="logos:facebook" size="xl" color="neutral" variant="outline" square to="https://facebook.com/"
+        <UButton icon="logos:facebook" size="xl" color="neutral" variant="outline" square to="https://www.facebook.com/ariffoysal.nayeem.9/"
             target="_blank" class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
         <UButton icon="ri:threads-fill" size="xl" color="neutral" variant="outline" square
             to="https://linkedin.com/in/yourusername" target="_blank"
