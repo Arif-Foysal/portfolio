0b86399a3579f7542afc03a72046a0fc599aafed
Arif Foysal <wayne11nayem@gmail.com>
2025-11-01 23:02:39 +0600

    social icons and prompt input updated

---
+++ app/components/SocialIcons.vue
@@ -0,0 +1,29 @@
+<template>
+    <UFieldGroup size="xl">
+        <UButton icon="mdi:github" size="xl"  color="neutral" variant="outline" square to="https://github.com/yourusername"
+            target="_blank" class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
+        <UButton icon="logos:google-gmail" size="xl" color="neutral" variant="outline" square
+            to="https://twitter.com/yourusername" target="_blank"
+            class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
+        <UButton icon="logos:whatsapp-icon" size="xl" color="neutral" variant="outline" square
+            to="https://linkedin.com/in/yourusername" target="_blank"
+            class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
+        <UButton icon="logos:linkedin-icon" size="xl" color="neutral" variant="outline" square
+            to="https://linkedin.com/in/yourusername" target="_blank"
+            class=" transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
+        <UButton icon="logos:facebook" size="xl" color="neutral" variant="outline" square to="https://facebook.com/"
+            target="_blank" class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
+        <UButton icon="ri:threads-fill" size="xl" color="neutral" variant="outline" square
+            to="https://linkedin.com/in/yourusername" target="_blank"
+            class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
+
+        <UButton icon="ri:twitter-x-line" size="xl" color="neutral" variant="outline" square
+            to="https://linkedin.com/in/yourusername" target="_blank"
+            class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1" />
+    </UFieldGroup>
+
+
+
+
+
+</template>
--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -1,31 +1,4 @@
 <template>
-  <UFieldGroup>
-    <UButton 
-      icon="i-simple-icons-twitter" 
-      color="neutral" 
-      variant="outline" 
-      square 
-      to="https://twitter.com/yourusername"
-      target="_blank"
-      class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1"
-    />
-    <UButton 
-      icon="i-simple-icons-github" 
-      color="neutral" 
-      variant="outline" 
-      square 
-      to="https://github.com/yourusername"
-      target="_blank"
-      class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1"
-    />
-    <UButton 
-      icon="i-simple-icons-linkedin" 
-      color="neutral" 
-      variant="outline" 
-      square 
-      to="https://linkedin.com/in/yourusername"
-      target="_blank"
-      class="transition-transform duration-200 hover:scale-110 hover:-translate-y-1"
-    />
-  </UFieldGroup>
+<SocialIcons />
+
 </template>
